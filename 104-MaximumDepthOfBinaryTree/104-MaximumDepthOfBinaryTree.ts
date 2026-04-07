64adbe097d444d7364ecae24e87466b820738904
Caramel Macchiato <ekanuraga@gmail.com>
2026-04-08 02:08:36 +0700

    Create 104-MaximumDepthOfBinaryTree

---
+++ 104-MaximumDepthOfBinaryTree/104-MaximumDepthOfBinaryTree.ts
@@ -0,0 +1,22 @@
+// Last updated: 4/8/2026, 2:08:35 AM
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if (!root) {
+        return 0 
+    } else {
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
+    }
+};
