824a983a17773f564784877dd7f9e6105c3c9a66
Caramel Macchiato <ekanuraga@gmail.com>
2026-04-08 02:07:39 +0700

    Create 300-LongestIncreasingSubsequence

---
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.ts
@@ -0,0 +1,18 @@
+// Last updated: 4/8/2026, 2:07:38 AM
+function lengthOfLIS(nums: number[]): number {
+    return getLIS(nums)
+};
+
+const getLIS = (nums: number[]): number => {
+    if (nums.length === 0) return 0;
+    let lis = Array(nums.length).fill(1);
+
+    for (let i = nums.length - 2; i >=0; i--) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) {
+                lis[i] = Math.max(lis[i], 1 + lis[j])
+            }
+        }
+    }
+    return Math.max(...lis)
+}
