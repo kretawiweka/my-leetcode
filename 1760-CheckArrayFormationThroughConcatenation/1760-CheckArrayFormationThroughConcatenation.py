4795197271b1919ca56bd74d10a3f8026794b54d
Caramel Macchiato <ekanuraga@gmail.com>
2026-04-08 02:06:06 +0700

    Create 1760-CheckArrayFormationThroughConcatenation

---
+++ 1760-CheckArrayFormationThroughConcatenation/1760-CheckArrayFormationThroughConcatenation.py
@@ -0,0 +1,11 @@
+# Last updated: 4/8/2026, 2:06:05 AM
+class Solution:
+    def canFormArray(self, arr: List[int], pieces: List[List[int]]) -> bool:
+        mp = {x[0]: x for x in pieces}
+        res = []
+        
+       
+        for num in arr:
+            res += mp.get(num, [])
+        
+        return res == arr
