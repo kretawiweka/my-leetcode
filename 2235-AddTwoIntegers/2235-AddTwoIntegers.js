158cc24ada80497f317140310ce9f7762d9d0709
Caramel Macchiato <ekanuraga@gmail.com>
2026-04-24 20:41:39 +0700

    Create 2235-AddTwoIntegers

---
+++ 2235-AddTwoIntegers/2235-AddTwoIntegers.js
@@ -0,0 +1,9 @@
+// Last updated: 4/24/2026, 8:41:39 PM
+1/**
+2 * @param {number} num1
+3 * @param {number} num2
+4 * @return {number}
+5 */
+6var sum = function(num1, num2) {
+7    return num1+num2
+8};
