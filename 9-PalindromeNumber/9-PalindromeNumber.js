fe719382bf7fa4c5e80955d2a1cf40398d388145
Caramel Macchiato <ekanuraga@gmail.com>
2026-04-27 21:48:33 +0700

    File updated at 4/27/2026, 9:48:32 PM

--- 9-PalindromeNumber/9-PalindromeNumber.js
+++ 9-PalindromeNumber/9-PalindromeNumber.js
@@ -1,17 +1,23 @@
-// Last updated: 4/8/2026, 2:09:21 AM
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(input) {
-      let reversedArr = input.toString().split('')
-
-  for (i=0;i<(reversedArr.length-1)/2;i++){
-    let a = reversedArr[i]
-    reversedArr[i] = reversedArr[(reversedArr.length-1)-i]
-    reversedArr[(reversedArr.length-1)-i] = a
-  }
-  let reversed = parseInt(reversedArr.join(''))
-
-  return input === reversed
-};
+// Last updated: 4/27/2026, 9:48:32 PM
+1/**
+2 * @param {number} x
+3 * @return {boolean}
+4 */
+5var isPalindrome = function(x) {
+6    if (x < 0) return false
+7
+8    let arr = x.toString().split('')
+9    let left = 0
+10    let right = arr.length - 1
+11
+12    while(left < right) {
+13        if (arr[left] !== arr[right]) {
+14            return false
+15        }
+16
+17        left++
+18        right--
+19    }
+20
+21    return true
+22};
