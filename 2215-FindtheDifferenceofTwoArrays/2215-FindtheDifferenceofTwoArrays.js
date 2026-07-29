ddbd901bbd85700ebb2d82a13de0620460d7bcd2
Caramel Macchiato <ekanuraga@gmail.com>
2026-07-30 05:50:15 +0700

    File updated at 7/30/2026, 5:50:15 AM

--- 2215-FindtheDifferenceofTwoArrays/2215-FindtheDifferenceofTwoArrays.js
+++ 2215-FindtheDifferenceofTwoArrays/2215-FindtheDifferenceofTwoArrays.js
@@ -1,29 +1,27 @@
-// Last updated: 7/30/2026, 5:48:40 AM
+// Last updated: 7/30/2026, 5:50:15 AM
 1/**
 2 * @param {number[]} nums1
 3 * @param {number[]} nums2
 4 * @return {number[][]}
 5 */
 6var findDifference = function(nums1, nums2) {
-7    let res1 = []
-8    let res2 = []
+7    const set1 = new Set(nums1);
+8    const set2 = new Set(nums2);
 9
-10    for (let i = 0; i < nums1.length; i++) {
-11        if (nums2.indexOf(nums1[i]) === -1) {
-12            if (res1.indexOf(nums1[i]) === -1) {
-13                res1.push(nums1[i])
-14            }
-15            
+10    const onlyIn1 = [];
+11    const onlyIn2 = [];
+12
+13    for (const num of set1) {
+14        if (!set2.has(num)) {
+15            onlyIn1.push(num);
 16        }
 17    }
 18
-19    for (let i = 0; i < nums2.length; i++) {
-20         if (nums1.indexOf(nums2[i]) === -1) {
-21            if (res2.indexOf(nums2[i]) === -1) {
-22                res2.push(nums2[i])
-23            }
-24        }
-25    }
-26
-27    return [res1, res2]
-28};
+19    for (const num of set2) {
+20        if (!set1.has(num)) {
+21            onlyIn2.push(num);
+22        }
+23    }
+24
+25    return [onlyIn1, onlyIn2];
+26};
